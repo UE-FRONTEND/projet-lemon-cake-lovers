f080cb7c1689402bbbfe6b83eef96018aa6b78bf
Amaury De La Rochebrochard <amaury.Dlr@hotmail.fr>
2021-11-23 11:26:11 +0100

    Link => button

--- src/Components/Home.vue
+++ src/Components/Home.vue
@@ -2,11 +2,12 @@
     <div>
       <p>Groupe : De La Rochebrochard Amaury et Boumali Samy</p>
       <p>TPA51</p>
-      <input type="button" value="Démarrer"  class="col"/>
       <input type="button" value="Consulter historique" class="col"/>
     </div>
     <div>
-      <router-link to="/Partie"> PARTIE </router-link>
+      <router-link to="/Partie" tag="button">
+        <input type="button" value="Démarrer"  class="col"/>
+      </router-link>
       <router-view></router-view>
     </div>
 </template>
