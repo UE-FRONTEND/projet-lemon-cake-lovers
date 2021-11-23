d7fedff2d3fce09b0daf848897008fe0a7e5ed58
Boumali Samy <samy.boumali@hotmail.com>
2021-11-24 00:52:30 +0100

    Code agencé, Pb récup code réponse au guess

--- README.md
+++ README.md
@@ -33,3 +33,6 @@ npm run test
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Date rendu 
+22/12/2021
--- src/Components/Partie.vue
+++ src/Components/Partie.vue
@@ -1,16 +1,24 @@
 <template>
-  {{ minute }} : {{second}}
+  <input id ="GuessText" type="text"/>
+  <br/>
+  <input type="button" value="Submit" @click="submit"/>
+  <p>{{ minute }} : {{second}}</p>
 </template>
 
+
+
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       minute : 10,
       second : 0,
+      token : this.getToken(),
+      guess : undefined
     }
   },
-  mounted() {
+  mounted : function() {
     this.$nextTick(function () {
 
       window.setInterval(() => {
@@ -27,5 +35,30 @@ export default {
       },1000);
     })
   },
+  methods : {
+    getToken : async function ()  {
+      let res;
+      await axios
+          .get("https://vuejs-rest-challenge.herokuapp.com/token")
+          .then( (response) => {
+            res = response.data;
+            //Debug
+            //console.log(response.data);
+          })
+          .catch(function(error) {
+            window.console.log(error);
+          });
+      this.token = res.token;
+      //window.console.log(this.token);
+    },
+    submit : async function () {
+      this.guess = parseInt(document.getElementById("GuessText").value);
+      window.console.log(this.guess);
+      let answer;
+      let response1 = axios.post("https://vuejs-rest-challenge.herokuapp.com/try",
+                 {token : this.token , guess : this.guess});
+      window.console.log(response1);
+    }
+  }
 }
 </script>
