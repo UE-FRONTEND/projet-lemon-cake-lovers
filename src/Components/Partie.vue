267d4249d566cc5d92df0961638795cc4e1f481a
Amaury De La Rochebrochard <amaury.Dlr@hotmail.fr>
2021-11-23 11:58:30 +0100

    get token

--- src/Components/Partie.vue
+++ src/Components/Partie.vue
@@ -3,28 +3,45 @@
 </template>
 
 <script>
+
+import axios from "axios";
+
 export default {
   data() {
     return {
       minute : 10,
       second : 0,
+      token : undefined,
+      res : undefined
     }
   },
-  mounted() {
-    this.$nextTick(function () {
+  //Appeler à l'affichage de la page
+  async mounted() {
+    //Requete
+    await axios
+        .get("https://vuejs-rest-challenge.herokuapp.com/token")
+        .then( (response) => {
+          this.res = response.data;
+          //Debug
+          //console.log(response.data);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+        this.token = this.res.token;
+    //Timer
+    await this.$nextTick(function () {
 
       window.setInterval(() => {
         if (this.second > 0) {
           this.second--;
-        }
-        else if (this.second === 0 && this.minute > 0) {
+        } else if (this.second === 0 && this.minute > 0) {
           this.second = 59;
           this.minute--;
-        }
-        else {
+        } else {
           // Timer hits 0 do what you want to do here.
         }
-      },1000);
+      }, 1000);
     })
   },
 }
