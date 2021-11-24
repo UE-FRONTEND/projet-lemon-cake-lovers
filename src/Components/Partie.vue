bb14dff2632c3597ba87afbd22b7f734eab42423
Amaury De La Rochebrochard <amaury.Dlr@hotmail.fr>
2021-11-24 23:45:01 +0100

    Refactored code / handled return val of post request / tried to center timer

--- src/Components/Partie.vue
+++ src/Components/Partie.vue
@@ -1,91 +1,100 @@
 <template>
+  <h1>{{ minute }} : {{second}}</h1>
   <input id ="GuessText" type="text"/>
   <br/>
   <input type="button" value="Submit" @click="submit"/>
-  <p>{{ minute }} : {{second}}</p>
+  <p>{{txt}}</p>
 </template>
 
 
 
 <script>
-<<<<<<< HEAD
 
 import axios from "axios";
 
-=======
-import axios from "axios";
->>>>>>> d7fedff2d3fce09b0daf848897008fe0a7e5ed58
 export default {
   data() {
     return {
       minute : 10,
       second : 0,
-<<<<<<< HEAD
       token : undefined,
-      res : undefined
+      res : undefined,
+      txt : "Tentez votre chance !",
+      intervalFunc : undefined, //contain the interval for the timer
     }
   },
-  //Appeler à l'affichage de la page
-  async mounted() {
-    //Requete
-    await axios
-        .get("https://vuejs-rest-challenge.herokuapp.com/token")
-        .then( (response) => {
-          this.res = response.data;
-          //Debug
-          //console.log(response.data);
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-        this.token = this.res.token;
-    //Timer
-    await this.$nextTick(function () {
-=======
-      token : this.getToken(),
-      guess : undefined
-    }
-  },
-  mounted : function() {
-    this.$nextTick(function () {
->>>>>>> d7fedff2d3fce09b0daf848897008fe0a7e5ed58
 
-      window.setInterval(() => {
+  //Called at page render
+  mounted : function() {
+    //Init token for request
+    this.getToken();
+
+    this.$nextTick( () => this.tickFunc() ); //Leave the function otherwise js will use the void return of tickfunc
+  },
+
+  methods : {
+    //Decrease the timer and set intervalFunc
+    tickFunc: function(){
+      this.intervalFunc = window.setInterval(() => {
         if (this.second > 0) {
           this.second--;
+
         } else if (this.second === 0 && this.minute > 0) {
           this.second = 59;
           this.minute--;
+
         } else {
           // Timer hits 0 do what you want to do here.
         }
       }, 1000);
-    })
-  },
-  methods : {
-    getToken : async function ()  {
-      let res;
+    },
+
+    getToken : async function() {
+      //Get Request
       await axios
           .get("https://vuejs-rest-challenge.herokuapp.com/token")
           .then( (response) => {
-            res = response.data;
+            this.res = response.data;
             //Debug
             //console.log(response.data);
           })
           .catch(function(error) {
-            window.console.log(error);
+            console.log(error);
           });
-      this.token = res.token;
-      //window.console.log(this.token);
+      this.token = this.res.token;
     },
+
     submit : async function () {
+      //Post request
       this.guess = parseInt(document.getElementById("GuessText").value);
-      window.console.log(this.guess);
-      let answer;
       let response1 = axios.post("https://vuejs-rest-challenge.herokuapp.com/try",
                  {token : this.token , guess : this.guess});
-      window.console.log(response1);
+      let ret_val = (await response1).data.code; //response return a tab with field code
+
+      //Debug
+      window.console.log(this.guess);
+      window.console.log(ret_val);
+
+      //Handling req value
+
+      if(ret_val === 1){
+        this.txt = "C'est plus !";
+
+      }else if(ret_val === 0){
+        this.txt = "Bravo !";
+        clearInterval(this.intervalFunc);
+
+      }else if(ret_val === -1){
+        this.txt = "C'est moins !";
+      }
     }
   }
 }
 </script>
+
+<style scoped>
+
+h1 {
+  text-align:center;
+}
+</style>
